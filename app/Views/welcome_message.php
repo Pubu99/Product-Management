e5fa95a709c32b4b8d9a192b8e523724c9be3d96
Pubu99 <123052759+Pubu99@users.noreply.github.com>
2024-07-23 16:55:38 +0530

    2

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Modern HTML Template</title>
-    <meta name="description" content="Product Management">
+    <title>Product Management</title>
+    <meta name="description" content="Product Management of SunQuick Sri Lanka">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
 
